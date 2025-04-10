aa3c87f2cbdce4ab59f2a3ce205c1e46b75f49a2
minkyung01 <84274527+minkyung01@users.noreply.github.com>
2025-04-10 09:53:32 +0900

    update door opening direction

--- Assets/HW03_2076298_LMK/Door1_Controller.cs
+++ Assets/HW03_2076298_LMK/Door1_Controller.cs
@@ -6,13 +6,13 @@ public class Door1_Controller : MonoBehaviour
 {
     public Animator animator;
 
-    void Start()
-    {
-        animator.speed = 0f;
-    }
-
     private void OnTriggerEnter(Collider other)
     {
+        float localZ = transform.InverseTransformPoint(other.transform.position).z;
+        int status = (localZ > 0) ? 1 : 2;
+
+        animator.SetInteger("door1_status", status);
+        
         animator.speed = 1f;
     }
 
